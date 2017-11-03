738d2cc5abe766105879d7f76ab742f8677b4e9c
Diego Jara <saintplay96@gmail.com>
2017-11-03 00:51:14 -0500

    add Divider - Change filename To PascalCase

---
+++ js/components/shared/Divider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default () => (
+  <View
+    style={{
+      borderBottomColor: '#A8A8A8',
+      borderBottomWidth: 1,
+      marginTop: 9,
+    }}
+  />
+);
